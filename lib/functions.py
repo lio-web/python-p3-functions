0aa9854b554cc4d6178e975169b79709d68fcf6b
lionard <leonard.muhati@student.moringaschool.com>
2024-09-24 09:52:33 +0300

    compelete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+# Call the function and assign its return value to a variable
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if isinstance(number, (int, float)):
+        return number/2
+    else:
+        return None
